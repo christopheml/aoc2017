e9b790ec9ff5ac5f339328cbc9609937876a5245
Christophe Michel <christopheml@users.noreply.github.com>
2017-12-02 23:29:23 +0100

    Algorithm implementation.

--- src/main/java/day1/InverseCaptcha.java
+++ src/main/java/day1/InverseCaptcha.java
@@ -6,7 +6,26 @@ public final class InverseCaptcha {
     }
 
     public static int compute(String captcha) {
-        return 0;
+        int total = 0;
+
+        for (int i = 0; i < captcha.length(); i++) {
+            char current = captcha.charAt(i);
+            char next = captcha.charAt(nextPosition(captcha, i));
+            if (current == next) {
+                total += Character.getNumericValue(current);
+            }
+        }
+
+        return total;
+    }
+
+    /**
+     * Returns the position next to the given one in the given captcha.
+     *
+     * This wraps around the end of the string, so nextPosition(captcha, captcha.size() - 1) == 0.
+     */
+    private static int nextPosition(String captcha, int i) {
+        return (i + 1) % captcha.length();
     }
 
 }
