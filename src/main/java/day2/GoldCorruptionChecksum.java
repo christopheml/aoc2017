ec7b19b277908d22ccfc0bb3258611a684edc1ba
Christophe Michel <christopheml@users.noreply.github.com>
2017-12-03 01:35:37 +0100

    [day2] Test case for the second half of day 2.

---
+++ src/main/java/day2/GoldCorruptionChecksum.java
@@ -0,0 +1,14 @@
+package day2;
+
+import java.util.List;
+
+public final class GoldCorruptionChecksum {
+
+    private GoldCorruptionChecksum() {
+    }
+
+    public static int rowChecksum(List<Integer> row) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/day2/GoldCorruptionChecksumTest.java
@@ -0,0 +1,39 @@
+package day2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Parameterized.class)
+public class GoldCorruptionChecksumTest {
+
+    @Parameterized.Parameters(name = "{0} => {1}")
+    public static Collection<Object[]> testData() {
+        return asList(new Object[][]{
+                {asList(5, 9, 2, 8), 4},
+                {asList(9, 4, 7, 3), 3},
+                {asList(3, 8, 6, 5), 2},
+        });
+    }
+
+    private final List<Integer> row;
+
+    private final int expectedChecksum;
+
+    public GoldCorruptionChecksumTest(List<Integer> row, int expectedChecksum) {
+        this.row = row;
+        this.expectedChecksum = expectedChecksum;
+    }
+
+    @Test
+    public void testInputs() {
+        assertThat(GoldCorruptionChecksum.rowChecksum(row)).isEqualTo(expectedChecksum);
+    }
+
+}
