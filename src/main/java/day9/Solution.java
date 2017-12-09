621081f61c1e6cb18380903cdf4741e3331786c4
Christophe Michel <christopheml@users.noreply.github.com>
2017-12-09 14:01:18 +0100

    [day9] Solution for both parts.

---
+++ src/main/java/day9/Solution.java
@@ -0,0 +1,33 @@
+package day9;
+
+import common.FileReader;
+import common.Node;
+
+import java.util.List;
+
+public final class Solution {
+
+    private Solution() {
+    }
+
+    public static void main(String... args) throws Exception {
+        try (FileReader reader = FileReader.read("/day9/stream.txt")) {
+            String stream = reader.lines().findFirst().orElseThrow(IllegalStateException::new);
+
+            StreamTokenizer tokenizer = new StreamTokenizer(stream);
+            StreamParser parser = new StreamParser();
+            List<Token> tokens = tokenizer.tokenize();
+            Node<Integer> root = parser.parse(tokens);
+
+            ScoreCalculator scoreCalculator = new ScoreCalculator();
+            int totalScore = scoreCalculator.assignScores(root);
+
+            System.out.println("Total score for the stream is: " + totalScore);
+
+            long garbageCount = tokens.stream().filter(token -> token == Token.GARBAGE).count();
+            System.out.println("Total garbage found: " + garbageCount);
+        }
+
+    }
+
+}
