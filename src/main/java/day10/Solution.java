590afa96825546f95fdd91df61f0185989e76e7c
Christophe Michel <christopheml@users.noreply.github.com>
2017-12-10 18:30:10 +0100

    [day10] Solution.

--- src/main/java/day10/Solution.java
+++ src/main/java/day10/Solution.java
@@ -8,11 +8,15 @@ final class Solution {
     }
 
     public static void main(String... args) {
-        KnotHash hash = new KnotHash(256,
+        KnotHash partialHash = new KnotHash(256,
                 asList(212, 254, 178, 237, 2, 0, 1, 54, 167, 92, 117, 125, 255, 61, 159, 164));
-        hash.round();
-        int hashValue = hash.sample();
-        System.out.println("Hash value is: " + hashValue);
+        partialHash.round();
+        int sample = partialHash.sample();
+        System.out.println("Hash sample is: " + sample);
+
+        AdvancedKnotHash knotHash = new AdvancedKnotHash("212,254,178,237,2,0,1,54,167,92,117,125,255,61,159,164");
+        String hash = knotHash.compute();
+        System.out.println("Hash value is: " + hash);
     }
 
 }
