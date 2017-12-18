0d8462f718223f4063dc42c9457b5f9f444e207d
Christophe MICHEL <christopheml@users.noreply.github.com>
2017-12-18 13:21:42 +0100

    [day16] Parsing once and deferring execution of steps (down to 10000 steps in 3917ms)

--- src/main/java/day16/Dance.java
+++ src/main/java/day16/Dance.java
@@ -1,33 +1,41 @@
 package day16;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
 public class Dance {
 
-    private final String steps;
+    private final List<Consumer<Line>> steps;
 
     public Dance(String steps) {
-        this.steps = steps;
+        this.steps = parse(steps);
     }
 
-    public void perform(Line line) {
-        String[] actions = steps.split(",");
-        for (String action : actions) {
-            performAction(line, action);
-        }
+    private List<Consumer<Line>> parse(String steps) {
+        String[] individualSteps = steps.split(",");
+        return Arrays.stream(individualSteps).map(this::parseStep).collect(Collectors.toList());
     }
 
-    private void performAction(Line line, String action) {
-        String parameters = action.substring(1);
-        if (action.startsWith("s")) {
+    private Consumer<Line> parseStep(String step) {
+        String parameters = step.substring(1);
+        if (step.startsWith("s")) {
             int offset = Integer.valueOf(parameters);
-            line.spin(offset);
-        } else if (action.startsWith("x")) {
+            return line -> line.spin(offset);
+        } else if (step.startsWith("x")) {
             String[] subparameters = parameters.split("/");
             int p1 = Integer.valueOf(subparameters[0]);
             int p2 = Integer.valueOf(subparameters[1]);
-            line.swapPositions(p1, p2);
-        } else if (action.startsWith("p")) {
-            line.swapPrograms(parameters.charAt(0), parameters.charAt(2));
+            return line -> line.swapPositions(p1, p2);
+        } else if (step.startsWith("p")) {
+            return line -> line.swapPrograms(parameters.charAt(0), parameters.charAt(2));
         }
+        throw new IllegalArgumentException();
+    }
+
+    public void perform(Line line) {
+        steps.forEach(step -> step.accept(line));
     }
 
 }
--- src/main/java/day16/Solution.java
+++ src/main/java/day16/Solution.java
@@ -7,6 +7,7 @@ final class Solution {
     public static void main(String... args) throws Exception {
         String steps = input();
         firstPartSolution(steps);
+        secondPartSolution(steps);
     }
 
     private static void firstPartSolution(String steps) {
@@ -17,6 +18,19 @@ final class Solution {
         dance.perform(line);
         System.out.println("Time: " + (System.currentTimeMillis() - l) + "ms");
         System.out.println("Line after dancing: " + line);
+    }
+
+    private static void secondPartSolution(String steps) {
+        Line line = new Line(16);
+        Dance dance = new Dance(steps);
+
+        long l = System.currentTimeMillis();
+        int iterations = 10000;
+        for (int i = 0; i < iterations; ++i) {
+            dance.perform(line);
+        }
+        System.out.println("Time: " + (System.currentTimeMillis() - l) + "ms");
+        System.out.println("Line after dancing " + iterations + " iterations: " + line);
     }
 
     private static String input() throws Exception {
