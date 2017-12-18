5ab732ea0f9cfe70cc21e470433ec0cb6149b193
Christophe MICHEL <christopheml@users.noreply.github.com>
2017-12-18 13:52:14 +0100

    [day16] No string manipulation and in-place array operations whenever possible (down to 10000 steps in 2500ms)

--- src/main/java/day16/Line.java
+++ src/main/java/day16/Line.java
@@ -1,46 +1,67 @@
 package day16;
 
+import java.util.Arrays;
+
 class Line {
 
     private static final String PROGRAMS = "abcdefghijklmnop";
 
-    private String programs;
+    private final char[] programs;
 
     public Line(int size) {
         if (size > PROGRAMS.length()) {
             throw new IllegalArgumentException();
         }
 
-        programs = PROGRAMS.substring(0, size);
+        programs = PROGRAMS.substring(0, size).toCharArray();
     }
 
     public void spin(int offset) {
-        int leftChuckSize = programs.length() - (offset % programs.length());
+        int leftChunkSize = programs.length - (offset % programs.length);
+        int rightChunkSize = offset % programs.length;
 
-        String leftChuck = programs.substring(0, leftChuckSize);
-        String rightChuck = programs.substring(leftChuckSize);
-
-        programs = rightChuck + leftChuck;
+        if (leftChunkSize > rightChunkSize) {
+            // Saving the biggest chunk to make room for in-place array copy
+            char[] leftChunk = Arrays.copyOf(programs, leftChunkSize);
+            // Copy of right chunk to the left of the array
+            System.arraycopy(programs, leftChunkSize, programs, 0, rightChunkSize);
+            // Copy of left chunk to the right of the array
+            System.arraycopy(leftChunk, 0, programs, rightChunkSize, leftChunkSize);
+        } else {
+            // Saving the biggest chunk to make room for in-place array copy
+            char[] rightChunk = new char[rightChunkSize];
+            System.arraycopy(programs, leftChunkSize, rightChunk,0, rightChunkSize);
+            // Copy of left chunk to the right of the array
+            System.arraycopy(programs, 0, programs, rightChunkSize, leftChunkSize);
+            // Copy of right chunk to the left of the array
+            System.arraycopy(rightChunk, 0, programs, 0, rightChunkSize);
+        }
     }
 
     public void swapPositions(int p1, int p2) {
-        char[] chars = programs.toCharArray();
-        char temp = chars[p1];
-        chars[p1] = chars[p2];
-        chars[p2] = temp;
-
-        programs = new String(chars);
+        char temp = programs[p1];
+        programs[p1] = programs[p2];
+        programs[p2] = temp;
     }
 
     public void swapPrograms(char c1, char c2) {
-        int p1 = programs.indexOf(c1);
-        int p2 = programs.indexOf(c2);
+        int p1 = indexOf(c1);
+        int p2 = indexOf(c2);
         swapPositions(p1, p2);
+    }
+
+    private int indexOf(char c) {
+        for (int i = 0; i < programs.length; ++i) {
+            if (programs[i] == c) {
+                return i;
+            }
+        }
+        throw new IllegalStateException();
     }
 
     @Override
     public String toString() {
-        return programs;
+        return new String(programs);
     }
 
 }
