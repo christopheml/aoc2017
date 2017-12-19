3b8a8558203ddeb022bd9402867fea8b4b64c1a9
Christophe MICHEL <christopheml@users.noreply.github.com>
2017-12-19 14:47:44 +0100

    [day18] Recover instruction in disabled mode.

--- src/main/java/day18/Parser.java
+++ src/main/java/day18/Parser.java
@@ -1,25 +1,40 @@
 package day18;
 
+import java.util.function.Supplier;
+
 public class Parser {
 
     public Instruction parse(String input) {
         String[] parts = input.split(" ");
         switch (parts[0]) {
             case "snd":
-                return sound(Integer.valueOf(parts[1]));
+                return sound(argument(parts[1]));
             case "set":
             case "add":
             case "mul":
             case "mod":
             case "rcv":
+                return recover(argument(parts[1]));
             case "jgz":
             default:
                 throw new UnsupportedOperationException("Unknown instruction " + parts[0]);
         }
     }
 
-    private Instruction sound(int frequency) {
-        return virtualMachine -> virtualMachine.sound(frequency);
+    private Supplier<Integer> argument(String part) {
+        return () -> Integer.valueOf(part);
+    }
+
+    private Instruction recover(Supplier<Integer> argument) {
+        return virtualMachine -> {
+            if (argument.get() > 0) {
+                virtualMachine.recover();
+            }
+        };
+    }
+
+    private Instruction sound(Supplier<Integer> frequency) {
+        return virtualMachine -> virtualMachine.sound(frequency.get());
     }
 
 }
--- src/test/java/day18/ParserTest.java
+++ src/test/java/day18/ParserTest.java
@@ -8,14 +8,24 @@ import static org.mockito.Mockito.verify;
 
 public class ParserTest {
 
+    private final Parser parser = new Parser();
+
     @Test
     public void sound() throws Exception {
-        Parser parser = new Parser();
         Instruction instruction = parser.parse("snd 18");
         VirtualMachine vm = Mockito.mock(VirtualMachine.class);
 
         instruction.accept(vm);
         verify(vm, times(1)).sound(18);
+    }
+
+    @Test
+    public void recover_inactive() throws Exception {
+        Instruction instruction = parser.parse("rcv 0");
+        VirtualMachine vm = Mockito.mock(VirtualMachine.class);
+
+        instruction.accept(vm);
+        verify(vm, times(0)).recover();
     }
 
 }
