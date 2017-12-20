3119f4236b859f356141f7204d7041bf2c3c57ce
Christophe MICHEL <christopheml@users.noreply.github.com>
2017-12-20 10:51:56 +0100

    [day18] Solution (faulty)

---
+++ src/main/java/day18/Solution.java
@@ -0,0 +1,20 @@
+package day18;
+
+import common.FileReader;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+final class Solution {
+
+    public static void main(String... args) throws Exception {
+        try (FileReader reader = FileReader.read("/day18/program.txt")) {
+            Parser parser = new Parser();
+            List<Instruction> program = reader.lines().map(parser::parse).collect(Collectors.toList());
+            FirstRecoverVirtualMachine vm = new FirstRecoverVirtualMachine();
+            int solution = vm.executeUntilFirstRecover(program);
+            System.out.println("The first recovered frequency is: " + solution);
+        }
+    }
+
+}
